f38bc2f7ef82172d495f465b9341fd3b8ee0a2ed
Alex <34625198+al3xfischer@users.noreply.github.com>
2021-09-05 20:02:14 +0200

    setup: add install nvim plugins

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,6 @@
 [System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', 'C:/Users/Alex/.config',[System.EnvironmentVariableTarget]::User)
+
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 setx XDG_CONFIG_HOME c:/tools/config
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
