a7545d6ddb1df5d6649e51ffcb739939d6dfb007
Alex <34625198+al3xfischer@users.noreply.github.com>
2021-09-05 17:34:28 +0200

    setup: add packer installation

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,3 @@
 [System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', 'C:/Users/Alex/.config',[System.EnvironmentVariableTarget]::User)
+git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+
--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 setx XDG_CONFIG_HOME c:/tools/config
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
