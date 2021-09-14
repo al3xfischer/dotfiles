6bfc8a70cbbcd84d6c01431ed4a144eefbc817f0
Alexander Fischer <alex.fischer@live.at>
2021-09-14 10:41:33 +0200

    linux setup rm set xdg_config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-setx XDG_CONFIG_HOME C:/Users/Alex/.config
+#setx XDG_CONFIG_HOME C:/Users/Alex/.config
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
